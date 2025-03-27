1868468f5946552f30f3723b3cd497997054203c
MrWooly125 <belov.20.04@mail.ru>
2025-03-27 20:14:32 +0300

    Changed quickMove.

--- src/main/java/net/mrwooly357/medievalstuff/screen/custom/heaters/CopperstoneHeaterScreenHandler.java
+++ src/main/java/net/mrwooly357/medievalstuff/screen/custom/heaters/CopperstoneHeaterScreenHandler.java
@@ -32,14 +32,13 @@ public class CopperstoneHeaterScreenHandler extends AbstractHeaterScreenHandler 
     }
 
 
-    /*@Override
+    @Override
     public ItemStack quickMove(PlayerEntity player, int invSlot) {
         ItemStack newStack = ItemStack.EMPTY;
         Slot slot = this.slots.get(invSlot);
         if (slot.hasStack()) {
             ItemStack originalStack = slot.getStack();
             newStack = originalStack.copy();
-
             if (invSlot < this.inventory.size()) {
                 if (!this.insertItem(originalStack, this.inventory.size(), this.slots.size(), true)) {
                     return ItemStack.EMPTY;
@@ -48,99 +47,14 @@ public class CopperstoneHeaterScreenHandler extends AbstractHeaterScreenHandler 
                 return ItemStack.EMPTY;
             }
 
-
-        }
-
-        return newStack;
-    }*/
-
-    @Override
-    public ItemStack quickMove(PlayerEntity player, int invSlot) {
-        ItemStack newStack = ItemStack.EMPTY;
-        Slot slot = this.slots.get(invSlot);
-        if (slot.hasStack()) {
-            ItemStack stackInSlot = slot.getStack();
-            newStack = stackInSlot.copy();
-
-            if (stackInSlot.isEmpty() && invSlot == 0) {
-
-                if (!this.insertItem(stackInSlot, 0, 37, true)) {
-                    return ItemStack.EMPTY;
-                }
-
-                slot.onQuickTransfer(stackInSlot, newStack);
-
-            } else if (invSlot != 0) {
-
-                if (this.isFuel(stackInSlot)) {
-
-                    if (!this.insertItem(stackInSlot, 0, 1, false)) {
-
-                        return ItemStack.EMPTY;
-                    }
-
-                    slot.onQuickTransfer(stackInSlot, newStack);
-
-                } else if (invSlot >= 1 && invSlot < 28) {
-
-                    if (!this.insertItem(stackInSlot, 28, 37, false)) {
-                        return ItemStack.EMPTY;
-                    }
-
-                } else if (invSlot >= 28 && invSlot < 37 && !this.insertItem(stackInSlot, 1, 28, false)) {
-                    return ItemStack.EMPTY;
-                }
-
-            } else if (!this.insertItem(stackInSlot, 0, 37, false)) {
-                return ItemStack.EMPTY;
-            }
-
-            if (stackInSlot.isEmpty()) {
+            if (originalStack.isEmpty()) {
                 slot.setStack(ItemStack.EMPTY);
-
             } else {
                 slot.markDirty();
             }
-
-            if (stackInSlot.getCount() == newStack.getCount()) {
-                return ItemStack.EMPTY;
-            }
-
-            slot.onTakeItem(player, stackInSlot);
         }
-
         return newStack;
     }
-
-    /*@Override
-    public ItemStack quickMove(PlayerEntity player, int invSlot) {
-        ItemStack newStack = ItemStack.EMPTY;
-        Slot slot = this.slots.get(invSlot);
-
-        if (slot.hasStack()) {
-            ItemStack stackInSlot = slot.getStack();
-            newStack = stackInSlot.copy();
-
-            if (stackInSlot.isEmpty()) {
-
-                if (!this.insertItem(stackInSlot, 0, 37, true)) {
-                    return ItemStack.EMPTY;
-                }
-            } else {
-                if (!this.insertItem(stackInSlot, 1, 37, true)) {
-                    return ItemStack.EMPTY;
-                }
-            }
-
-            if (stackInSlot.isEmpty()) {
-                slot.setStack(ItemStack.EMPTY);
-            } else {
-                slot.markDirty();
-            }
-        }
-
-        return newStack;
-    }*/
 
     @Override
     public boolean canUse(PlayerEntity player) {
